6302b864d6329938d50634fb7dce1a4a551376c4
anonWilder <austineokafor.e@gmail.com>
2022-12-27 03:11:15 +0100

    Wrote the code for the header section. imported data for looping through my various social media icon and profile

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,28 @@
+import HeaderImage from '../../assets/HeaderPhoto.jpg'
+import data from './data'
 import './header.css'
 
 const Header = () => {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header id="header">
+      <div className='container header__container'>
+        <div className='header__profile'>
+          <img src={HeaderImage} alt="Header Portrait" />
+        </div>
+        <h3>Egbonu Kenechukwu</h3>
+        <p>You are clicks away from building your dream website or web app. Send me the details of your project for a modern, mobile responsive, highly performant website today!
+        </p>
+        <div className='header__cta'>
+          <a href="#contact" className='btn primary'>Lets Talk</a>
+          <a href="#portfolio" className='btn light'>My Work</a>
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel='noopener noreferer'>{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,17 @@
+import {AiOutlineInstagram} from "react-icons/ai";
+import {AiOutlineTwitter} from "react-icons/ai";
+import {AiOutlineFacebook} from "react-icons/ai";
+import {AiOutlineMedium} from "react-icons/ai";
+import {AiOutlineLinkedin} from "react-icons/ai";
+import {AiFillGithub} from "react-icons/ai";
+
+const data = [
+    {id: 1, link: 'https://www.instagram.com/egbonu_kenechukwu/', icon: <AiOutlineInstagram/>},
+    {id: 1, link: 'https://twitter.com/Kenechi__', icon: <AiOutlineTwitter/>},
+    {id: 1, link: 'https://www.facebook.com/julys.august/', icon: <AiOutlineFacebook/>},
+    {id: 1, link: 'https://medium.com/@austineokafor.e', icon: <AiOutlineMedium/>},
+    {id: 1, link: 'https://www.linkedin.com/in/kenechukwu-egbonu-640748230/', icon: <AiOutlineLinkedin/>},
+    {id: 1, link: 'https://github.com/anonWilder', icon: <AiFillGithub/>}
+    ]
+
+    export default data
